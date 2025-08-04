37b1ea9c07ce16e18ed279e4d60787b69b1f46d9
rahull0328 <rmmehta2004@gmail.com>
2025-08-04 18:10:51 +0530

    added validations for login and register

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,52 +1,77 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import Input from '../../components/Input/Input';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Input/Input";
+import { validateEmail } from "../../utils/helper";
 
-const Login = ({setCurrentPage}) => {
-
+const Login = ({ setCurrentPage }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const handleLogin = async(e) => {
-    e.preventDefault()
-  }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    //login api call
+    try {
+      
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
+  };
   return (
-    <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
-      <h3 className='text-lg font-semibold text-black'>Welcome Back</h3>
-      <p className='text-xs text-slate-700 mt-[5px] mb-6'>Please enter your details to log in</p>
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Please enter your details to log in
+      </p>
 
       <form onSubmit={handleLogin}>
-        <Input 
+        <Input
           value={email}
-          onChange={({ target}) => setEmail(target.value)}
+          onChange={({ target }) => setEmail(target.value)}
           label="Email Address"
           placeholder="john@example.com"
           type="text"
         />
 
-        <Input 
+        <Input
           value={password}
-          onChange={({target}) => setPassword(target.value)}
+          onChange={({ target }) => setPassword(target.value)}
           label="Password"
           placeholder="Min 8 Characters"
-          type='password'
+          type="password"
         />
 
-        {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
 
-        <button className='btn-primary' type='submit'>
+        <button className="btn-primary" type="submit">
           Login
         </button>
 
-        <p className='text-[13px] text-slate-800 mt-3'>
+        <p className="text-[13px] text-slate-800 mt-3">
           Don't have an account?{" "}
           <button
-            className='font-medium text-primary underline cursor-pointer'
+            className="font-medium text-primary underline cursor-pointer"
             onClick={() => {
-              setCurrentPage("register")
+              setCurrentPage("register");
             }}
           >
             Register
@@ -54,7 +79,7 @@ const Login = ({setCurrentPage}) => {
         </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,71 +1,97 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Input from '../../components/Input/Input'
-import ProfilePhotoSelector from '../../components/Input/ProfilePhotoSelector'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Input/Input";
+import ProfilePhotoSelector from "../../components/Input/ProfilePhotoSelector";
+import { validateEmail } from "../../utils/helper";
 
-const Register = ({setCurrentPage}) => {
+const Register = ({ setCurrentPage }) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [profilePic, setProfilePic] = useState(null)
-  const [fullName, setFullName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   //handling register event
   const handleRegister = async (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!fullName) {
+      setError("Please enter full name...");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address...");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter password...");
+      return;
+    }
+
+    setError("");
+
+    //register api call
+    try {
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
+  };
   return (
-    <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
-      <h3 className='text-lg font-semibold text-black'>Create Account</h3>
-      <p className='text-xs text-slate-700 mt-[5px] mb-6'>
-        Join us today!
-      </p>
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Create Account</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">Join us today!</p>
 
       <form onSubmit={handleRegister}>
-        <div className='grid grid-cols-1 md:grid-cols-1 gap-2'>
-
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-2">
           <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
 
-          <Input 
+          <Input
             value={fullName}
-            onChange={({target}) => setFullName(target.value)}
+            onChange={({ target }) => setFullName(target.value)}
             label="Full Name"
             placeholder="John Doe"
             type="text"
           />
 
-          <Input 
+          <Input
             value={email}
-            onChange={({target}) => setEmail(target.value)}
+            onChange={({ target }) => setEmail(target.value)}
             label="Email Address"
             placeholder="john.doe@example.com"
             type="email"
           />
 
-          <Input 
+          <Input
             value={password}
-            onChange={({target}) => setPassword(target.value)}
+            onChange={({ target }) => setPassword(target.value)}
             label="Password"
             placeholder="Min 8 characters"
             type="password"
           />
 
-          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
 
-          <button type='submit' className='btn-primary'>
+          <button type="submit" className="btn-primary">
             Register
           </button>
 
-          <p className='text-[13px] text-slate-800 mt-3'>
+          <p className="text-[13px] text-slate-800 mt-3">
             Already have an Account?{" "}
             <button
-              className='font-medium text-primary underline cursor-pointer'
+              className="font-medium text-primary underline cursor-pointer"
               onClick={() => {
-                setCurrentPage("login")
+                setCurrentPage("login");
               }}
             >
               Login
@@ -74,7 +100,7 @@ const Register = ({setCurrentPage}) => {
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
